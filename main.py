55e89400d974da8a50bc3386b4c2051ea5ee6b54
Pisit Kaewfung <38246181+BluebirdLyrase@users.noreply.github.com>
2024-06-27 15:32:40 +0700

    set up simple websocket

---
+++ main.py
@@ -0,0 +1,13 @@
+import asyncio
+import websockets
+
+async def echo(websocket, path):
+    async for message in websocket:
+        await websocket.send(message)
+        await websocket.send(path)
+
+async def main():
+    async with websockets.serve(echo, "localhost", 8765):
+        await asyncio.Future()  # run forever
+
+asyncio.run(main())
