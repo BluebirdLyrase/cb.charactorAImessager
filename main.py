1f4119cfaa009d65ebacf17f62f9278dc69439e7
Pisit Kaewfung <38246181+BluebirdLyrase@users.noreply.github.com>
2024-06-27 16:10:57 +0700

    finish project

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
 import asyncio
+import os
+from characterai import aiocai
 import websockets
+from dotenv import load_dotenv, dotenv_values 
+
+load_dotenv() 
+print("service start")
 
 async def echo(websocket, path):
-    async for message in websocket:
-        await websocket.send(message)
-        await websocket.send(path)
+    char_id = path[1:]
+    client_id = os.getenv("CLIENT_ID")
+    client = aiocai.Client(client_id)
+    me = await client.get_me()
+    async with await client.connect() as chat:
+        new, answer = await chat.new_chat(
+            char_id, me.id
+        )
+        await websocket.send(f'{answer.name}: {answer.text}')
+        async for message in websocket:
+            message_ai = await chat.send_message(
+                char_id, new.chat_id, message
+            )
+            await websocket.send(f'{message_ai.name}: {message_ai.text}')
+
+
 
 async def main():
     async with websockets.serve(echo, "localhost", 8765):
         await asyncio.Future()  # run forever
+
+
 
 asyncio.run(main())
